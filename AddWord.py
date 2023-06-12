90fb4dbe5c8f02c25c8fd152224124090ea42670
KicamonIce <1532931259@qq.com>
2023-06-13 00:56:05 +0800

    DuplicarteCheck

--- AddWord.py
+++ AddWord.py
@@ -2,14 +2,26 @@
 
 import pyperclip
 
-path = "/home/KicamonIce/Documents/study/Note/English/MyWords.txt"
+path = "/home/KicamonIce/Documents/study/Note/English"
 NewWord = pyperclip.paste()
 
 
 def write_word(path, newword):
     path += "/MyWords.txt"
+    with open(path, "r") as f:
+        line = f.readline()
+        while line:
+            hadword = ""
+            for char in line:
+                if char == '.':
+                    break
+                hadword += char
+            if hadword == newword:
+                return
+            line = f.readline()
+    addstr = f"{newword}.<++>.1."
     with open(path, 'a+') as f:
-        f.writelines(newword)
+        f.writelines(addstr+'\n')
 
 
-write_word(path, f"{NewWord}.<++>.1.")
+write_word(path, NewWord)
